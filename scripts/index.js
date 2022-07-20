1b6d59a2b6be3cd526bbe046a748a982fcd7a08f
Zeu Capua <zeucapua@gmail.com>
2022-07-19 20:31:48 -0700

    Add files via upload
    final commit, implemented all features including:
    - search engine
    - weather based on location
    - use current location
    - 5 day forecast
    - unit conversions between metric and imperial

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ let tempDisplay = document.querySelector(".weather-temp");
 let cityDisplay = document.querySelector(".city-display");
 let windDisplay = document.querySelector(".bi-wind");
 let humidityDisplay = document.querySelector(".bi-droplet-half");
+let forecastDisplay = document.querySelector(".forecast");
 let weatherDesc = document.querySelector(".weather-desc");
 let weatherIcon = document.querySelector(".weather-icon");
 
@@ -41,6 +42,22 @@ function getCurrentTime() {
   return `${day} ${time}`;
 }
 
+function getDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+  console.log(date.getDay());
+  return day;
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
@@ -52,6 +69,7 @@ function searchCity(city) {
   cityDisplay.innerHTML = city;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(getTemperature);
+  axios.get(apiUrl).then(getCityCoordinates)
 }
 
 function getCurrentLocation(event) {
@@ -64,6 +82,7 @@ function searchCurrentLocation(position) {
   let long = position.coords.longitude;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(setCurrentLocation);
+  getForecast(lat,long);
 }
 
 function setCurrentLocation(response) {
@@ -80,6 +99,55 @@ function getTemperature(response) {
   setHumidity(response);
   setWeatherDisplay(response);
 }
+
+function getCityCoordinates(response) {
+  let data = response.data;
+  let lat = data.coord.lat;
+  let long = data.coord.lon;
+  getForecast(lat,long);
+}
+
+function getForecast(lat,long) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=${units}&appid=${apiKey}&exclude=current,minutely,hourly,alerts`;
+  axios.get(apiUrl).then(setForecast);
+}
+
+function setForecast(response) {
+  let data = response.data;
+  let forecasts = data.daily;
+  console.log(forecasts);
+  let cards = "";
+  forecasts.forEach(function (forecast, index) {
+    if (index < 5) {
+      let timestamp = forecast.dt;
+      let min = forecast.temp.min;
+      let max = forecast.temp.max;
+      let iconCode = forecast.weather[0].icon;
+      let iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+      let forecastCard = `
+        <div class="col-sm-2">
+          <div class="card">
+            <div class="card-body">
+              <img 
+                class="forecast-icon"
+                src=${iconUrl}
+              />
+              <h5 class="card-title">${max}°</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${min}°</h6>
+              <p class="card-text">${getDay(timestamp)}</p>
+            </div>
+          </div>
+        </div>
+      `;
+      cards += forecastCard;
+      forecastDisplay.innerHTML = cards;
+    }
+    
+  });
+  
+}
+
+
 
 function setWeatherDisplay(response) {
   let data = response.data;
@@ -107,16 +175,12 @@ function setTemperature(temperature) {
   tempDisplay.innerHTML = result;
 }
 
-function setMetric() {
-  if (units != "metric") { units = "metric"; searchCity(city); }
-}
+function setMetric() { if (units != "metric") { units = "metric"; searchCity(city); } }
 
-function setImperial() {
-  if (units != "imperial") { units = "imperial"; searchCity(city); }
-}
+function setImperial() { if (units != "imperial") { units = "imperial"; searchCity(city); } }
 
 // connect functions to html elements
-timeDisplay.innerHTML = getCurrentTime();
+timeDisplay.innerHTML = "Your Time: " + getCurrentTime();
 searchForm.addEventListener("submit", searchSubmit);
 celsiusBtn.addEventListener("click", setMetric);
 fahrenheitBtn.addEventListener("click", setImperial);
